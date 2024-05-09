f74cf9007de67a70b832581bff3074a1e1345529
Symevich <163895676+Symevich@users.noreply.github.com>
2024-05-09 20:17:07 +0300

    start

---
+++ script.js
@@ -0,0 +1,43 @@
+function one() {
+  document.getElementById("screen").innerText += "1";
+}
+
+function two() {
+  document.getElementById("screen").innerText += "2";
+}
+
+function three() {
+  document.getElementById("screen").innerText += "3";
+}
+
+function four() {
+  document.getElementById("screen").innerText += "4";
+}
+
+function five() {
+  document.getElementById("screen").innerText += "5";
+}
+
+function six() {
+  document.getElementById("screen").innerText += "6";
+}
+
+function seven() {
+  document.getElementById("screen").innerText += "7";
+}
+
+function eight() {
+  document.getElementById("screen").innerText += "8";
+}
+
+function nine() {
+  document.getElementById("screen").innerText += "9";
+}
+
+function zero() {
+  document.getElementById("screen").innerText += "0";
+}
+
+function clear1() {
+  document.getElementById("screen").innerText = "";
+}
